c5b899e8dca2987b8ce918e11c840ee36904c1c3
Samuel Meyers <thecheesecakejunkie@gmail.com>
2020-10-16 11:17:04 -0500

    Section 6

--- main.js
+++ main.js
@@ -99,3 +99,31 @@ for(let person of people) {
     }
 }
 
+/**
+ * Section 6
+ */
+console.log("--------------- Section 6 ---------------");
+
+let cipher = {
+    a: 2,
+    b: 3,
+    c: 4,
+    d: 5
+};
+
+function decode(word) {
+    if(ciher[word[0]]) {
+        return word[cipher[word[0]]];
+    } else {
+        return ' ';
+    }
+}
+
+function decodeWords(words) {
+    let list = words.split(' ');
+    let decoded = "";
+    for(let word of list) {
+        decoded += decode(word);
+    }
+}
+
