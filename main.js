fc8ff282462c50e7d54c5f5fdb6a023b7d032dcb
Samuel Meyers <thecheesecakejunkie@gmail.com>
2020-10-16 10:46:01 -0500

    Section 2

--- main.js
+++ main.js
@@ -10,3 +10,17 @@ let loaf = {
 
 console.log("Flour:", loaf.flour, "Water:", loaf.water);
 console.log("Hydration:", loaf.hydration());
+
+let weird = {
+    foo: 42,
+    bar: "Hello, world!",
+    fum: ['fe', 'fi', 'fo'],
+    quux: { name: "Quark" },
+    spam: function() {
+        console.log("Email");
+    }
+};
+
+for(let key in weird) {
+    console.log("Key:", key, "Value:", weird[key]);
+}
